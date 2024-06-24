9d90350622cfb927280157a2e1bf626db65a5e76
frashealey <fraser42a@gmail.com>
2024-06-24 23:37:35 +0100

    Create tracks and return errors
    Steps 2, 5, 6, 7, adds valid documents and returns errors for invalid ones

--- src/app.js
+++ src/app.js
@@ -8,14 +8,16 @@
 * @author Fraser Healey.
 */
 
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const xlsx = require("xlsx");
-const { Track, Contract } = require("./models/models")
+const { Track, Contract } = require("./models/models");
+dotenv.config();
 
 /**
 * Opens Excel file, splits Aliases by semicolon, and returns array of objects.
-* @param  {String}     filename    Excel file to parse
-* @return {Array}                  Parsed tracks data as Array
+* @param {String} filename - Excel file to parse.
+* @return {Array} Parsed tracks data as Array.
 */
 function parseImport(filename) {
     // Catches wrong filetype
@@ -23,10 +25,10 @@ function parseImport(filename) {
         return false;
     };
 
-    // Opens and parses Excel file (removing first 'annotations' column)
+    // Opens and parses Excel file (removing first annotations column)
     const excelFile = xlsx.readFile(filename);
     let excelParsed = xlsx.utils.sheet_to_json(excelFile["Sheets"][excelFile.SheetNames[0]]).slice(1);
-    // Separates alises, by semicolon, for each track
+    // Separates aliases by semicolon for each track
     return excelParsed.map((i) => {
         i["Aliases"] = i["Aliases"].replace(/\s/g, "").split(";"); 
         return i;
@@ -34,20 +36,87 @@ function parseImport(filename) {
 };
 
 /**
-* Main function for the script.
+* Starts a transaction, checks if "Contract" exists (if provided),
+* ingests track if it does (or no "Contract" is provided),
+* and returns error if it does not.
+* @param  {Object} track - Track object to be ingested.
+* @return {Promise<String>} Error string if track cannot be ingested.
 */
-function main() {
-    console.log(Track);
-    console.log(Contract);
+async function ingestTrack(track) {
+    let ingestError;
+    let session = await mongoose.startSession();
 
-    console.log(parseImport("task/Track Import Test.xlsx"));
+    try {
+        // Starts transaction, executes, and commits (or aborts on error)
+        ingestError = await session.withTransaction(async () => {
+            // Contract name exists in input track
+            if ("Contract" in track) {
+                // Finds matching contract name, and returns
+                // error if it does not exist in database
+                let queryContract = await Contract.findOne(
+                    { "Name": track["Contract"] }
+                ).session(session);
+                if (queryContract === null) {
+                    return `${track["Contract"]} is not found`;
+                };
+
+                // Remove contract field and add contract ID field
+                // (if contract exists in database)
+                delete track["Contract"];
+                track["Contract ID"] = queryContract["_id"];
+            }
+
+            // Creates Track document
+            await Track.create([ track ], { session });
+        });
+    }
+    finally {
+        await session.endSession();
+    };
+
+    return ingestError;
+};
+
+/**
+* Main function for the script.
+* @return {Promise<Array>} Array of errored tracks.
+*/
+async function main() {
+    // Opens and parses the spreadsheet
+    const data = parseImport("task/Track Import Test.xlsx");
+    if (data === false) {
+        return;
+    };
+
+    // Connects to local database
+    await mongoose.connect(process.env.DB_HOST);
+
+    // Creates Contract 1
+    await Contract.create({ "Name": "Contract 1" });
+
+    // Ingests or pushes error for each track
+    let ingestErrorArr = [];
+    for (let i = 0, n = data.length; i < n; i++) {
+        const ingestError = await ingestTrack(data[i]);
+        // Pushes error if one is produced
+        if (ingestError) {
+            ingestErrorArr.push({ "Line": i, "Error": ingestError });
+        };
+    };
+    // Logs errors to console
+    console.table(ingestErrorArr);
+
+    // Disconnects from database when finished
+    await mongoose.disconnect();
+    return ingestErrorArr;
 };
 
 if (require.main === module) {
     main();
 };
 
-// Export functions to be used for tests
 module.exports = {
-    parseImport
+    parseImport,
+    ingestTrack,
+    main
 };
--- test/test.js
+++ test/test.js
@@ -1,46 +1,125 @@
 const _ = require("lodash");
 const assert = require("assert");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = require("../src/app");
+const { Track, Contract } = require("../src/models/models");
+dotenv.config();
 
-describe("Excel", function() {
-    describe("Non-Excel file", function() {
-        it("returns false when no file", function() {
-            assert.equal(app.parseImport(""), false);
-        });
-        it("returns false when incorrect filetype", function() {
-            assert.equal(app.parseImport("task/Coding Test Details.docx"), false);
-        });
+describe("Excel tests", function() {
+    it("returns false when no file", function() {
+        assert.equal(app.parseImport(""), false);
+    });
+    it("returns false when incorrect filetype", function() {
+        assert.equal(app.parseImport("task/Coding Test Details.docx"), false);
+    });
+    it("returns parsed data when valid filetype and format", function() {
+        assert(
+            _.isEqual(
+                app.parseImport("task/Track Import Test.xlsx"),
+                [
+                    {
+                        "Title": "Track 1",
+                        "Version": "Version 1",
+                        "Artist": "Artist 1",
+                        "ISRC": "ISRC1",
+                        "P Line": "P Line 1",
+                        "Aliases": [ "aliases1", "aliases2" ],
+                        "Contract": "Contract 1"
+                    },
+                    {
+                        "Title": "Track 2",
+                        "Version": "Version 2",
+                        "Artist": "Artist 2",
+                        "ISRC": "ISRC2",
+                        "P Line": "P Line 2",
+                        "Aliases": [ "aliases11", "aliases22" ],
+                        "Contract": "Contract 2"
+                    }
+                ]
+            )
+        );
+    });
+});
+
+describe("Ingest tests", function() {
+    before(function() {
+        return mongoose.connect(process.env.DB_HOST);
+    });
+    beforeEach(function() {
+        return Contract.create({ "Name": "testContractMatch" });
+    });
+    afterEach(function() {
+        return mongoose.connection.dropDatabase();
+    });
+    after(function() {
+        return mongoose.disconnect();
     });
 
-    describe("Valid file", function() {
-        it("returns parsed data when valid filetype and format", function() {
-            // Asserts that the value returned from parseImport matches
-            // the desired output
-            assert(
-                _.isEqual(
-                    app.parseImport("task/Track Import Test.xlsx"),
-                    [
-                        {
-                            Title: "Track 1",
-                            Version: "Version 1",
-                            Artist: "Artist 1",
-                            ISRC: "ISRC1",
-                            "P Line": "P Line 1",
-                            Aliases: [ "aliases1", "aliases2" ],
-                            Contract: "Contract 1"
-                        },
-                        {
-                            Title: "Track 2",
-                            Version: "Version 2",
-                            Artist: "Artist 2",
-                            ISRC: "ISRC2",
-                            "P Line": "P Line 2",
-                            Aliases: [ "aliases11", "aliases22" ],
-                            Contract: "Contract 2"
-                        }
-                    ]
-                )
-            );
-        });
+    it("ingests track when no contract field", async function() {
+        const ingestError = await app.ingestTrack(
+            {
+                "Title": "testTrack",
+                "Version": "testVersion",
+                "Artist": "testArtist",
+                "ISRC": "ISRCtest",
+                "P Line": "testPLine",
+                "Aliases": [ "testAlias" ],
+            }
+        );
+        const queryTrack = await Track.findOne();
+        assert(ingestError === undefined && queryTrack !== null);
+    });
+    it("returns error when contract field, contract does not exist", async function() {
+        const ingestError = await app.ingestTrack(
+            {
+                "Title": "testTrack",
+                "Version": "testVersion",
+                "Artist": "testArtist",
+                "ISRC": "ISRCtest",
+                "P Line": "testPLine",
+                "Aliases": [ "testAlias" ],
+                "Contract": "testContract"
+            }
+        );
+        const queryTrack = await Track.findOne();
+        assert(ingestError === "testContract is not found" && queryTrack === null);
+    });
+    it("ingests track when contract field, contract exists", async function() {
+        const ingestError = await app.ingestTrack(
+            {
+                "Title": "testTrack",
+                "Version": "testVersion",
+                "Artist": "testArtist",
+                "ISRC": "ISRCtest",
+                "P Line": "testPLine",
+                "Aliases": [ "testAlias" ],
+                "Contract": "testContractMatch"
+            }
+        );
+        const queryTrack = await Track.findOne();
+        assert(ingestError === undefined && queryTrack !== null);
+    });
+});
+
+describe("System test", function() {
+    it("ingests Track 1 and errors Track 2", async function() {
+        // Runs main function
+        const ingestErrorArr = await app.main();
+
+        // Verifies that only "Track 1" has been added
+        await mongoose.connect(process.env.DB_HOST);
+        const queryTrack = await Track.find();
+        await mongoose.connection.dropDatabase();
+        await mongoose.disconnect();
+
+        assert(
+            _.isEqual(
+                ingestErrorArr,
+                [ { Line: 1, Error: 'Contract 2 is not found' } ]
+            )
+            &&
+            ( queryTrack.length === 1 && queryTrack[0]["Title"] === "Track 1" )
+        );
     });
 });
